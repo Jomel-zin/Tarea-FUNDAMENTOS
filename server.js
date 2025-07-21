06992cb03a4d549a3f77030dae6fe9762e36fbd2
Jomel <jomelvera@icloud.com>
2025-07-20 22:22:46 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'index.html'));
+});
+// Servir archivos HTML (ej: login con Tailwind)
+app.use('/view', express.static('view'));
+
+
+
+// Conexión a la base de datos
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('✅ Conectado a la base de datos MySQL');
+});
+
+// Hacer disponible la conexión
+app.set('db', db);
+
+// Rutas
+app.use('/auth', authRoutes);
+
+// Iniciar el servidor
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${process.env.PORT}`);
+});
